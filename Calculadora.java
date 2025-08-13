abeeed0aa2da735fd69b7c136075c0d7f3df68cf
Deiby Enrique Calderon <164361682+Deibycalderon777@users.noreply.github.com>
2025-08-12 20:25:55 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -5,7 +5,6 @@ public class Calculadora {
         return total=primerN+segundoN;        
 
         }
-        
         public Double resta(Double primerN ,Double segundoN){
         return total=primerN-segundoN;        
      
@@ -17,9 +16,14 @@ public class Calculadora {
             
         }
         public Double divicion(Double primerN , Double segundoN){
-        return total=primerN/segundoN;        
-      
+         // antes de retornar el valor de total de la divicion comprobamos que segundoN no sea cero
+           // si, sies un 0 imprimimos en pantalla y no retornamos nada  
+        if (segundoN == 0) {
+            System.out.print("Error: Division por cero no permitida.");
+            return null;
         }
+        return total = primerN / segundoN;
+    }
 
 
  }// fin clase Calculadora
